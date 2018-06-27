fa3ede71facf38598de17768dd518623cac90325
Nelson Umunna <nelson.umunna@zetatango.com>
2018-06-27 12:29:05 -0400

    Added Hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "Hello, Nelson Umunna Welcome to Zetatango!"
+    end
 end
