2264a32df96e7681f25ff43707c59fd87f8f6cb3
Nelson Umunna <nelson.umunna@zetatango.com>
2018-06-28 18:27:59 -0400

    Finished layouts and routes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'bootstrap-sass', '3.3.7'
 gem 'rails',        '5.2.0'
 gem 'puma',         '3.9.1'
 gem 'sass-rails',   '5.0.6'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,22 +15,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test "should get home" do
-    get static_pages_home_url #get a url, Home and Help pages are ordinary urls accessed via GET request. 
+    get root_path    #get a url, Home and Help pages are ordinary urls accessed via GET request. 
     assert_response :success #The abstract representation of of the underlying HTTP status code ie 200
     #the code above checks for the presence of a <title> tag containing the string “Home | Ruby on Rails Tutorial Sample App”
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
